d88ef1403c2c8c704fa375945a4d4bc1f93d5203
Trịnh Tấn Minh <minh.207pm51630@vanlanguni.vn>
2023-08-25 18:29:25 +0700

    first commit

---
+++ assets/js/index.js
@@ -0,0 +1,10 @@
+window.addEventListener('scroll', function() {
+  const navbar = document.querySelector('.scroll');
+  const scrollPosition = window.scrollY;
+
+  if (scrollPosition > 0) {
+    navbar.classList.add('active');
+  } else {
+    navbar.classList.remove('active');
+  }
+});
